d576dbf5bf42f67246c9e97fd14db39550f1124f
Han Chiang <yaphc@hotmail.com>
2018-01-05 10:43:56 +0800

    Added test for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,10 +4,17 @@ const expect = require('expect');
 const { app } = require('../server');
 const { Todo } = require('../models/todo');
 
+const todos = [{
+  text: 'First test todo'
+}, {
+  text: 'Second test todo'
+}];
+
 beforeEach((done) => {
   // Make sure database is empty before every test
-  Todo.remove({})
-  .then(() => done());
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos)
+  }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -27,7 +34,7 @@ describe('POST /todos', () => {
         return done(err);
       }
       // Test todo added correctly to MongoDB
-      Todo.find().then(todos => {
+      Todo.find({text}).then(todos => {
         expect(todos.length).toBe(1);
         expect(todos[0].text).toBe(text);
         done();
@@ -47,10 +54,21 @@ describe('POST /todos', () => {
       }
 
       Todo.find().then(todos => {
-        expect(todos.length).toBe(0);
+        expect(todos.length).toBe(2);
         done()
       })
       .catch(err => done(err));
     });
+  });
+});
+
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+    request(app)
+    .get('/todos')
+    .expect(200)
+    .expect(response => {
+      expect(response.body.todos.length).toBe(2);
+    }).end(done);
   });
 });
