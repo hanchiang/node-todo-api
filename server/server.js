fe22c34748054beabf98ee85cf2d2715940b635b
Han Chiang <yaphc@hotmail.com>
2018-01-05 14:11:04 +0800

    Added PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
@@ -63,6 +64,30 @@ app.delete('/todos/:id', (request, response) => {
   .catch(err => response.status(400).send());
 });
 
+// PATCH /todos/:id
+app.patch('/todos/:id', (request, response) => {
+  const id = request.params.id;
+  if (!ObjectID.isValid(id)) {
+    response.status(400).send('ID is invalid');
+  }
+
+  let body = _.pick(request.body, ['text', 'completed']);
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true})
+  .then(todo => {
+    if (!todo) {
+      return response.status(404).send('ID not found');
+    }
+    response.send({todo});
+  })
+  .catch(err => response.status(400).send(err));
+});
 
 app.listen(port, () => console.log('Server is running on port ' + port));
 
